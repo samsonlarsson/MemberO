2195d7037819826816382719c4df20f72fedfd36
samsonpaul <samsonpaul4@gmail.com>
2017-01-11 14:43:16 +0200

    initialises the project

---
+++ app.py
@@ -0,0 +1,182 @@
+import pymongo    
+from psessionDA import psessionDAO
+from flask import Flask, url_for, redirect, render_template, request, session
+from flask_oauth import OAuth
+import json
+import os
+
+app = Flask(__name__)
+app.debug = True
+
+mongostr = os.environ['MONGODB_URI']
+
+con = pymongo.MongoClient(mongostr)
+DB = con.heroku_vfs22s9f
+dao = PsessionDAO(DB)
+registers = dao.GetRegisters()
+valid_constituencies = [c['constituency'] for c in registers]
+
+g_auth = DB.authorization.find_one({"name":"google"})
+
+# app.secret_key = g_auth["secret_key"]
+
+app.secret_key = os.environ['GOOGLE_CLIENT_SECRET']
+google = OAuth().remote_app('google',
+                        base_url='https://www.google.com/accounts/',
+                        authorize_url='https://accounts.google.com/o/oauth2/auth',
+                        request_token_url=None,
+                        request_token_params={
+                            'scope': 'https://www.googleapis.com/auth/userinfo.email',
+                            'response_type': 'code'
+                        },
+                        access_token_url='https://accounts.google.com/o/oauth2/token',
+                        access_token_method='POST',
+                        access_token_params={'grant_type': 'authorization_code'},
+                        #consumer_key=g_auth["consumer_key"],
+                        consumer_key=os.environ['GOOGLE_CLIENT_ID'],
+                        #consumer_secret=g_auth["consumer_secret"]
+                        consumer_secret=os.environ['GOOGLE_CLIENT_SECRET'])
+
+@app.route('/')
+def index():
+    access_token = session.get('access_token')
+    if access_token is None:
+        return redirect(url_for('login'))
+
+    access_token = access_token[0]
+    from urllib2 import Request, urlopen, URLError
+
+    headers = {'Authorization': 'OAuth '+access_token}
+    req = Request('https://www.googleapis.com/oauth2/v1/userinfo',
+                  None, headers)
+    try:
+        user_info = json.load(urlopen(req))
+    except URLError as e:
+        if e.code == 401:
+            # Unauthorized - bad token
+            session.pop('access_token', None)
+            return redirect(url_for('login'))
+        return "Error authorizing: " + json.dumps(user_info)
+
+    session["constituency"] = user_info.get("constituency")
+    return redirect(url_for("sessions_page",))
+
+@app.route('/login')
+def login():
+    callback=url_for('authorized', _external=True)
+    return google.authorize(callback=callback)
+
+@app.route("/oauth2callback")
+@google.authorized_handler
+def authorized(resp):
+    access_token = resp['access_token']
+    session['access_token'] = access_token, ''
+    return redirect(url_for('index'))
+
+@google.tokengetter
+def get_access_token():
+    return session.get('access_token')
+
+@app.route('/parliamentarians', methods=['POST', 'GET'])
+def parliamentarians_page():
+    access_token = session.get('access_token')
+    if access_token is None:
+        return redirect(url_for('login'))
+        
+    if request.method == 'POST':
+        name = request.form.get('name')
+        dob = request.form.get('dob')
+        emergencyphone = request.form.get('emergencyphone')
+        emergencycontact = request.form.get('emergencycontact')
+        gender = request.form.get('gender')
+        constituency = request.form.get('constituency')
+        dao.Addparliamentarian(name, dob, gender, constituency, emergencycontact, emergencyphone)
+    return render_template('parliamentarians.html', parliamentarians = dao.Getparliamentarians())
+
+@app.route('/parliamentarian_autocomplete')
+def parliamentarian_autocomplete():
+    access_token = session.get('access_token')
+    if access_token is None:
+        return redirect(url_for('login'))
+        
+    term = request.args.get("term")
+    return dao.Autocompleteparliamentarian(term)
+
+@app.route('/parliamentarian/<parliamentarian_id>', methods=['POST', 'GET'])
+def parliamentarian_page(parliamentarian_id):
+    access_token = session.get('access_token')
+    if access_token is None:
+        return redirect(url_for('login'))
+        
+    if request.method == 'POST':
+        name = request.form.get('name')
+        dob = request.form.get('dob')
+        emergencyphone = request.form.get('emergencyphone')
+        emergencycontact = request.form.get('emergencycontact')
+        gender = request.form.get('gender')
+        constituency = request.form.get('constituency')
+        dao.Editparliamentarian(name, dob, gender, constituency, emergencycontact, emergencyphone, parliamentarian_id)
+        return render_template('parliamentarians.html', parliamentarians = dao.Getparliamentarians())
+    return render_template('parliamentarian.html', parliamentarian = dao.Getparliamentarian(parliamentarian_id))
+
+@app.route('/edit_parliamentarian/<parliamentarian_id>', methods=['POST', 'GET'])
+def edit_parliamentarian(parliamentarian_id):
+    return render_template('edit_parliamentarian.html', parliamentarian = dao.Getparliamentarian(parliamentarian_id, edit=True))
+
+@app.route('/psessions', methods=['POST', 'GET'])
+def psessions_page():
+    access_token = session.get('access_token')
+    if access_token is None:
+        return redirect(url_for('login'))
+        
+    if request.method == 'POST':
+        register = request.form.get('register')
+        date = request.form.get('date')
+        ctype = request.form.get('type')
+        dao.AddPsession(register, date, ctype)
+    return render_template('psessions.html', psessions = dao.GetPsessions(), registers = dao.GetRegisters())
+
+@app.route('/remove_session/<session_id>', methods=['POST'])
+def remove_psession(psession_id):
+    access_token = session.get('access_token')
+    if access_token is None:
+        return redirect(url_for('login'))
+        
+    dao.RemovePsession(psession_id)
+    return redirect(url_for('psessions_page', psession_id=psession_id))
+
+@app.route('/session/<session_id>', methods=['POST', 'GET'])
+def psession_page(psession_id):
+    access_token = session.get('access_token')
+    if access_token is None:
+        return redirect(url_for('login'))
+        
+    if request.method == 'POST':
+        name = request.form.get('name')
+        payment = request.form.get('payment')
+        method = request.form.get('method')
+        ptype = request.form.get('type')
+        dao.AddPsessionAttendance(psession_id, name, payment, method, ptype)
+    return render_template('psession.html', psessionrec = dao.GetPsession(psession_id))
+
+@app.route('/remove_attendance/<psession_id>', methods=['POST'])
+def remove_attendance(psession_id):
+    access_token = session.get('access_token')
+    if access_token is None:
+        return redirect(url_for('login'))
+        
+    parliamentarian_id = request.form.get('parliamentarian_id')
+    dao.RemovePsessionAttendance(psession_id, parliamentarian_id)
+    return redirect(url_for('session_page', session_id=session_id))
+
+@app.before_request
+def check_auth():
+    constituency = session.get("constituency")
+    if valid_constituencies and constituency and (constituency not in valid_constituencies):
+        session.pop('access_token', None)
+        session.pop('constituency', None)
+        redirect(url_for('login'))
+
+if __name__ == "__main__":
+    app.run()
+
---
+++ psessionDA.py
@@ -0,0 +1,301 @@
+import json
+import re
+import pymongo
+from datetime import datetime
+from bson import json_util
+from bson.objectid import ObjectId
+
+class PsessionDAO():
+
+	def __init__(self, database):
+		self.db = database
+		self.parliamentariandb = database.parliamentarians
+		self.registerdb = database.registers
+		self.psessiondb = database.psessions
+
+	def CalcAge(self, birthday):
+		if birthday is None:
+			return ""
+
+		birthdate = datetime.now() - birthday
+		return int(birthdate.days/365.2425)
+
+	def ValidDate(self, date):
+		try:
+			return datetime.strptime(date, "%m/%d/%Y")
+		except:
+			return None
+
+	def GetRegisters(self):
+		co_recs = self.registerdb.find().sort("name",1)
+		if co_recs is None:
+			print("No registers in database?")
+			return None
+
+		registers = [{
+				"_id"  : c['_id'],
+				"name" : c['name'],
+				"constituency" : c['constituency']
+			} for c in co_recs]
+
+		return registers
+
+	def AutocompleteParliamentarian(self, name_start):
+		first, last = None, None
+		pieces = re.split("\W+", name_start)
+		if len(pieces) > 1:
+			first, last = pieces[0], " ".join(pieces[1:])
+		elif len(pieces) == 1:
+			first = pieces[0]
+		else:
+			return ""
+
+		first_regex = re.compile("^" + first, re.IGNORECASE)
+		last_regex = re.compile(("\w+" if last is None else "^"+last), re.IGNORECASE)
+
+		matches = self.parliamentariandb.aggregate([
+			{
+				"$match":{
+					"firstname":first_regex,
+					"lastname":last_regex
+				}
+			},
+			{
+				"$project":{
+					"name":{
+						"$concat":[
+							"$firstname",
+							" ",
+							"$lastname"
+						]
+					}
+				}
+			}
+		])
+
+		parliamentarians = [parliamentarian['name'] for parliamentarian in matches['result']]
+		return json.dumps(parliamentarians)
+
+	def AddPsessionAttendance(self, psessionid, name, house, method, ptype):		
+		first, last = None, None
+		pieces = re.split("\W+", name)
+		if len(pieces) > 1:
+			first, last = pieces[0], " ".join(pieces[1:])
+		else:
+			print("Name doesn't match pattern")
+			return None
+
+		stu_rec = self.parliamentariandb.find_one({'firstname':first, 'lastname':last})
+		if stu_rec is None:
+			print("parliamentarian does not exist")
+			return
+
+		att_rec = {"parliamentarian" : stu_rec['_id']}
+		if house != "":
+			att_rec.update({
+				"house" : {
+					"method" : method,
+					"house" : htype
+				}
+			})
+		elif ptype == "punched":
+			att_rec.update({
+				"house" : {
+					"method" : "punched",
+					"house" : None
+				}
+			})
+
+		self.psessiondb.update({
+					'_id':ObjectId(psessionid)
+				},
+				{
+					"$push":{
+						"attendance":att_rec
+					}
+				}
+			)
+
+	def RemovePsessionAttendance(self, psession_id, parliamentarian_id):
+		psessionrec = self.psessiondb.find_one({"_id":ObjectId(psession_id)})
+		attendance = [att for att in psessionrec['attendance']]
+		for i,att in enumerate(attendance):
+			if att['parliamentarian'] == ObjectId(parliamentarian_id):
+				del attendance[i]
+				break
+
+		psessionrec['attendance'] = attendance
+
+		self.psessiondb.update({
+				"_id":ObjectId(psession_id)
+			},
+			{
+				"$set":{
+					"attendance":attendance
+				}
+			})
+
+	def AddPsession(self, register, date, ctype):
+		co_rec = self.registerdb.find_one({'name':register})
+		if co_rec is None:
+			print("Register name not in system")
+			return
+		
+		psessiondate = self.ValidDate(date)
+		if psessiondate is None:
+			return None
+
+		new_psession = {
+			"date" : psessiondate,
+			"register" : co_rec['_id'],
+			"type" : ctype,
+			"attendance" : []
+		}
+		
+		cla_rec = self.psessiondb.find_one({'date':new_psession['date']})
+		if cla_rec is not None:
+			print("psession already exists")
+			return
+				
+		self.psessiondb.insert(new_psession)
+	
+	def RemovePsession(self, psession_id):
+		self.psessiondb.remove({"_id":ObjectId(psession_id)})
+
+	def GetPsession(self, psessionid):
+		psessionrec = self.psessiondb.find_one({"_id":ObjectId(psessionid)})
+
+		stud_ids = [rec["parliamentarian"] for rec in psessionrec["attendance"]]
+		dbparliamentarians = self.parliamentariandb.find({"_id":{"$in":stud_ids}})
+		parliamentarians = {parliamentarian['_id']:parliamentarian for parliamentarian in dbparliamentarians}
+
+		register = self.registerdb.find_one({"_id": psessionrec["register"]})
+
+		psessiondata = {
+			"id" : psessionrec['_id'],
+			"date": psessionrec['date'].strftime("%A, %B %d %Y"),
+			"register": register["name"],
+			"type": psessionrec["type"],
+			"attendance": []
+			# "notes": psessionrec["notes"]
+		}
+
+		for attrecord in psessionrec["attendance"]:
+			parliamentarian = parliamentarians[attrecord["parliamentarian"]]
+			name = parliamentarian["firstname"] + " " + parliamentarian["lastname"]
+
+			psessionrow = {
+				"id" : parliamentarian['_id'],
+				"name" : name,
+				"age" : self.CalcAge(parliamentarian["dob"]),
+				"gender" : parliamentarian['gender'],
+				"housed" : "",
+				"housemethod" : ""
+			}
+
+			if "house" in attrecord:
+				if attrecord["house"]["housed"] is not None:
+					psessionrow["housed"] = "$" + str(attrecord["house"]["amount"]) + " " + attrecord["house"]["housed"]
+				else:
+					psessionrow["housed"] = ""
+				psessionrow["housemethod"] = attrecord["house"]["method"]
+			psessiondata["attendance"].append(psessionrow)
+
+		return psessiondata
+
+	def GetPsessions(self):
+		dbpsessions = self.psessiondb.find().sort("date",-1)
+		dbregisters = self.registerdb.find()
+
+		registers = {register['_id']:register['name'] for register in dbregisters}
+
+		psessiontable = [{
+				"id": psessiond['_id'],
+				"register": registers[psessiond['register']],
+				"type" : psessiond["type"],
+				"date": psessiond['date'].strftime("%A, %B %d %Y"),
+				"attendance": len(psessiond["attendance"])
+			} for psessiond in dbpsessions]
+
+		return psessiontable
+
+	def EditParliamentarian(self, name, dob, gender, constituency, emergencycontact, emergencyphone, parliamentarian_id):
+		first, last = None, None
+		pieces = re.split("\W+", name)
+		if len(pieces) > 1:
+			first, last = pieces[0], " ".join(pieces[1:])
+		else:
+			print("Name doesn't match pattern")
+			return None
+
+		parliamentarian = {
+			"firstname" : first,
+			"lastname" : last,
+			"dob" : self.ValidDate(dob),
+			"constituency" : constituency,
+			"gender" : gender,
+			"emergencycontact" : emergencycontact,
+			"emergencyphone" : emergencyphone
+		}
+
+		self.parliamentariandb.update({'_id':ObjectId(parliamentarian_id)}, parliamentarian)
+
+	def AddParliamentarian(self, name, dob, gender, constituency, emergencycontact, emergencyphone):
+		first, last = None, None
+		pieces = re.split("\W+", name)
+		if len(pieces) > 1:
+			first, last = pieces[0], " ".join(pieces[1:])
+		else:
+			print("Name doesn't match pattern")
+			return None
+
+		parliamentarian = {
+			"firstname" : first,
+			"lastname" : last,
+			"dob" : self.ValidDate(dob),
+			"constituency" : constituency,
+			"gender" : gender,
+			"emergencycontact" : emergencycontact,
+			"emergencyphone" : emergencyphone
+		}
+
+		stu_rec = self.parliamentariandb.find_one({'firstname':first, 'lastname':last})
+		if stu_rec is not None:
+			print("parliamentarian already exists")
+			return
+
+		self.parliamentariandb.insert(parliamentarian)
+		
+	def GetParliamentarians(self):
+		dbparliamentarians = self.parliamentariandb.find({}).sort("lastname",1)
+
+		parliamentariantable = [{
+				"id" : stu_rec['_id'],
+				"name": stu_rec['firstname'] + " " + stu_rec['lastname'],
+				"age" : self.CalcAge(stu_rec["dob"]),
+				"gender" : stu_rec["gender"],
+				"constituency" : stu_rec["constituency"],
+			} for stu_rec in dbparliamentarians]
+
+		return parliamentariantable
+
+	def GetParliamentarian(self, parliamentarian_id, edit=False):
+		stu_rec = self.parliamentariandb.find_one({'_id':ObjectId(parliamentarian_id)})
+
+		dob_fmt = ("%m/%d/%Y" if edit else "%A, %B %d %Y")
+		birthdate = lambda x:(x.strftime(dob_fmt) if x else "") 
+
+		parliamentarian = {
+			"id": stu_rec['_id'],
+			"name": stu_rec['firstname'] + " " + stu_rec['lastname'],
+			"dob" : birthdate(stu_rec["dob"]),
+			"gender" : stu_rec["gender"],
+			"constituency" : stu_rec["constituency"],
+			"emergencycontact" : stu_rec["emergencycontact"],
+			"emergencyphone" : stu_rec["emergencyphone"]
+		}
+
+		return parliamentarian
+
+def j0(obj):return json.dumps(obj, sort_keys=True, indent=4, default=json_util.default)
+def jprint(obj):print(j0(obj))
